23004d44b764f8c4e8fa64116af847d88a4adf21
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2018-09-19 08:05:03 +0200

    Refactor paginator spec

--- spec/freckle_io/paginator_spec.rb
+++ spec/freckle_io/paginator_spec.rb
@@ -1,96 +1,66 @@
-require_relative '../spec_helper'
-require 'pry'
+require_relative "../spec_helper"
+require "pry"
 
-module FreckleIO
-  describe Paginator do
-    context 'with configuration', :vcr do
+describe FreckleIO::Paginator do
+  context "with configuration", :vcr do
+    before do
+      FreckleIO.reset
+      FreckleIO.configure do |config|
+        config.token = ENV["FRECKLE_TOKEN"]
+        config.auth_type = :freckle_token
+      end
+    end
+
+    let(:client) { FreckleIO::Client.new }
+    let!(:request) { client.get("/v2/users") }
+
+    describe "#next" do
       before do
-        FreckleIO.reset
-        FreckleIO.configure do |config|
-          config.token = ENV['FRECKLE_TOKEN']
-          config.auth_type = :freckle_token
-        end
+        paginator.next
       end
 
-      let(:client) { FreckleIO::Client.new }
-      let!(:request) { client.get("/v2/users") }
+      let(:paginator) { FreckleIO::Paginator.new(client.raw_links) }
 
-      describe "#next" do
-        before do
-          paginator.next
-        end
+      it "returns link of next page" do
+        expect(paginator.next).to eq("/v2/users?page=2")
+      end
+    end
 
-        let(:paginator) { FreckleIO::Paginator.new(client.raw_links) }
-        let(:next_ref) do
-          {:url=>"/v2/users?page=2", :rel=>"next", :number_page=>"2"}
-        end
-
-        it "returns ref of next page" do
-          expect(paginator.send(:pages)).to include(next_ref)
-        end
-
-        it "returns link of next page" do
-          expect(paginator.next).to eq("/v2/users?page=2")
-        end
+    describe "#prev" do
+      before do
+        client.last
+        paginator.prev
       end
 
-      describe "#prev" do
-        before do
-          client.last
-          paginator.prev
-        end
+      let(:paginator) { FreckleIO::Paginator.new(client.raw_links) }
 
-        let(:paginator) { FreckleIO::Paginator.new(client.raw_links) }
-        let(:prev_ref) do
-          {:url=>"/v2/users?page=1", :rel=>"prev", :number_page=>"1"}
-        end
+      it "returns link of previous page" do
+        expect(paginator.prev).to eq("/v2/users?page=1")
+      end
+    end
 
-        it "returns ref of previous page" do
-          expect(paginator.send(:pages)).to include(prev_ref)
-        end
-
-        it "returns link of previous page" do
-          expect(paginator.prev).to eq("/v2/users?page=1")
-        end
+    describe "#last" do
+      before do
+        paginator.last
       end
 
-      describe "#last" do
-        before do
-          paginator.last
-        end
+      let(:paginator) { FreckleIO::Paginator.new(client.raw_links) }
 
-        let(:paginator) { FreckleIO::Paginator.new(client.raw_links) }
-        let(:last_ref) do
-          {:url=>"/v2/users?page=2", :rel=>"last", :number_page=>"2"}
-        end
+      it "returns link of previous page" do
+        expect(paginator.last).to eq("/v2/users?page=2")
+      end
+    end
 
-        it "returns ref of previous page" do
-          expect(paginator.send(:pages)).to include(last_ref)
-        end
-
-        it "returns link of previous page" do
-          expect(paginator.last).to eq("/v2/users?page=2")
-        end
+    describe "#first" do
+      before do
+        client.last
+        paginator.first
       end
 
-      describe "#first" do
-        before do
-          client.last
-          paginator.first
-        end
+      let(:paginator) { FreckleIO::Paginator.new(client.raw_links) }
 
-        let(:paginator) { FreckleIO::Paginator.new(client.raw_links) }
-        let(:first_ref) do
-          {:url=>"/v2/users?page=1", :rel=>"first", :number_page=>"1"}
-        end
-
-        it "returns ref of previous page" do
-          expect(paginator.send(:pages)).to include(first_ref)
-        end
-
-        it "returns link of previous page" do
-          expect(paginator.first).to eq("/v2/users?page=1")
-        end
+      it "returns link of previous page" do
+        expect(paginator.first).to eq("/v2/users?page=1")
       end
     end
   end
