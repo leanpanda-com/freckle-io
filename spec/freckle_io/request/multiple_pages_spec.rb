98511565c5864e1b3051d00e4fdb48bd8cff68d1
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2018-10-24 18:14:39 +0200

    Wip: multiple pages spec

---
+++ spec/freckle_io/request/multiple_pages_spec.rb
@@ -0,0 +1,25 @@
+require_relative "../../spec_helper"
+
+
+describe FreckleIO::Request::MultiplePages do
+  context "with configuration", :vcr do
+    before do
+      FreckleIO.reset
+      FreckleIO.configure do |config|
+        config.token = ENV["FRECKLE_TOKEN"]
+        config.auth_type = :freckle_token
+      end
+    end
+
+    let(:subject) { described_class.new }
+    let(:users) { subject.get("/v2/users") }
+
+    describe "#get" do
+      let(:body_response) {}
+
+      it "returns an array of users" do
+        users
+      end
+    end
+  end
+end
