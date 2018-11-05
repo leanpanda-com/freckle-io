8c170f91a64a6bb69bf47e759605861ee41127f2
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2018-11-05 07:44:36 +0100

    Fix params

--- lib/freckle_io/request/multiple_pages.rb
+++ lib/freckle_io/request/multiple_pages.rb
@@ -3,35 +3,40 @@ require "faraday_middleware"
 module FreckleIO
   module Request
     class MultiplePages
+      attr_reader :path
+      attr_reader :params
       attr_reader :last_responses
 
       def get(path, params: {})
-        retrieve_all_pages(path, params: params)
+        @path ||= path
+        @params ||= default_params.merge(params)
+
+        retrieve_all_pages
 
         self
       end
 
       private
 
-      def retrieve_all_pages(path, params: {})
-        merged_params = default_params.merge(params)
+      def retrieve_all_pages
+        merged_params =
 
         @last_responses ||= client.get_in_parallel(
           path,
           2,
-          total_pages(path, params: merged_params),
-          params: merged_params
+          total_pages,
+          params: params
         )
 
         @last_responses.unshift([@first_response])
         @last_responses.flatten!
       end
 
-      def total_pages(path, params: {})
-        @total_pages ||= first_page(path, params: params).total_pages
+      def total_pages
+        @total_pages ||= first_page.total_pages
       end
 
-      def first_page(path, params: {})
+      def first_page
         @first_page ||= first_single_page.get(
           path,
           params: params
