1ecedba1cbbffc23830cc8a647df66d58e5a7bb6
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2018-11-20 19:36:09 +0100

    Add required files

--- lib/freckle_io.rb
+++ lib/freckle_io.rb
@@ -1,10 +1,16 @@
+require "dry-validation"
+
 require "freckle_io/configuration"
+require "freckle_io/connection"
+require "freckle_io/params"
 require "freckle_io/errors/configuration"
 require "freckle_io/errors/connection"
-require "freckle_io/connection"
-require "freckle_io/client/users"
-require "freckle_io/request/single_page"
+require "freckle_io/errors/params"
 require "freckle_io/request/multiple_pages"
+require "freckle_io/request/single_page"
+require "freckle_io/validator/restricted_hash"
+require "freckle_io/validator/user"
+require "freckle_io/client/users"
 
 module FreckleIO
   def self.configuration
