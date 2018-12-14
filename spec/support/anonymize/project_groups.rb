1b7436e2e19e62ca18e274f8b883a622e04287cc
Domenico Garofoli <d.garofoli@cantierecreativo.net>
2018-12-14 19:26:42 +0100

    Anonymize project groups

---
+++ spec/support/anonymize/project_groups.rb
@@ -0,0 +1,50 @@
+module Anonymize
+  class ProjectGroups
+    attr_accessor :interaction
+
+    # rubocop:disable Metrics/LineLength
+    PROJECT_GROUP_API_REPLACE_VALUES = {
+      id: /\"id\":(.*?),/mi,
+      name: /\"name\":\"(.*?)\"/mi,
+      formatted_name: /\"formatted_name\":\"(.*?)\"/mi,
+      url: /\"url\":\"(.*?)\"/mi,
+    }.freeze
+    # rubocop:enable Metrics/LineLength
+
+    def initialize(interaction:)
+      @interaction = interaction
+    end
+
+    def call
+      find_and_replace
+    end
+
+    private
+
+    def find_and_replace
+      PROJECT_GROUP_API_REPLACE_VALUES.each do |key, regex|
+        match_texts = interaction.response.body.scan(regex)
+
+        match_texts.each_with_index do |text, index|
+          replace = anonymize_response_value(key, index)
+          interaction.filter!(text.first, replace)
+        end
+      end
+
+      interaction
+    end
+
+    def anonymize_response_value(key, index)
+      return "http://foo.com/#{index}" if key.to_s.include? "url"
+
+      case key
+      when :id
+        index.to_s
+      when :mail
+        "#{key}_#{index}@domain.com"
+      else
+        "#{key}_#{index}"
+      end
+    end
+  end
+end
